746e525eca626094fe1145fde58c31572237df71
Fablet Julie <Juju35874@gmail.com>
2023-12-19 11:31:09 +0100

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://juliefablet.github.io',
+    site: 'https://JulieFablet.github.io',
 });
